92f644e8f6034ade97d60cfab9bd22b476acf58a
SabaAhmad404 <sabaahmad895@gmail.com>
2023-08-06 11:59:39 +0500

    Create database and APi

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ 
+
+Greeting.create(message: 'Hello, how are you?')
+Greeting.create(message: 'Hi, nice to meet you!')
+Greeting.create(message: 'Welcome to our app!')
+Greeting.create(message: 'Greetings and salutations!')
+Greeting.create(message: 'Hello there, stranger!')
---
+++ test/models/create_greeting_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CreateGreetingTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
